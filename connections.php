95ca5f8cfc128f2e4f8a769e38a9563d13b5fe08
johnhumbertduran <32027069+johnhumbertduran@users.noreply.github.com>
2018-07-12 13:34:08 +0800

    Add files via upload

---
+++ connections.php
@@ -0,0 +1,14 @@
+<?php
+
+
+	$connections = mysqli_connect("localhost", "root", "", "beastmodescore");
+	
+	if(mysqli_connect_errno()){
+
+		echo "Failed to connect to MySQL: " .mysqli_connect_error();
+
+	}
+
+
+
+?>

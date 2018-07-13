99403b410521822c146694e86d6c6868a0bab63d
johnhumbertduran <32027069+johnhumbertduran@users.noreply.github.com>
2018-07-13 19:22:35 +0800

    connections

--- connections.php
+++ connections.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 
@@ -12,19 +11,4 @@
 
 
 
-=======
-<?php
-
-
-	$connections = mysqli_connect("localhost", "root", "", "beastmodescore");
-	
-	if(mysqli_connect_errno()){
-
-		echo "Failed to connect to MySQL: " .mysqli_connect_error();
-
-	}
-
-
-
->>>>>>> 5b67d0acb712ce32603f101d86d5f09e6c8c3161
 ?>
